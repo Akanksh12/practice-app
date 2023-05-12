9550d1b3b71b883fc65a1113d560b0f1de6ff710
Akanksh12 <cleverakanksh@gmail.com>
2023-05-12 22:05:46 +0530

    removed uneccessary event listener from audio and clear upload on reload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 upload = document.querySelector('input')
 audioElem = document.querySelector('audio')
 
-upload.files = null //clears files on reload
+upload.value = null //clears files on reload
 upload.addEventListener('change', () => {
     audioElem.src = URL.createObjectURL(upload.files[0])
     console.log(audioElem.src)
